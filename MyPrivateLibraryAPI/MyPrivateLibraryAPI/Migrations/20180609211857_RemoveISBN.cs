407b24a7990f8dab58811ddbc86de439409d18a8
Aleksander Ligęza <aleksligeza@gmail.com>
2018-06-09 23:19:52 +0200

    Remove ISBN from the database

---
+++ MyPrivateLibraryAPI/MyPrivateLibraryAPI/Migrations/20180609211857_RemoveISBN.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyPrivateLibraryAPI.Migrations
+{
+    public partial class RemoveISBN : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Isbn",
+                table: "Books");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Isbn",
+                table: "Books",
+                nullable: true);
+        }
+    }
+}
